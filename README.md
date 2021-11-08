ef2ffa9d0fe829d743afb6420206d4aa772a2177
Yoko OYAMA <yowcow@gmail.com>
2021-11-08 11:57:49 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+buf
+===
+
+A command-line tool to read STDIN into a buffer, and print the buffer to STDOUT.
+
+HOW TO USE
+----------
+
+```
+buf | jq .
+{"hello":"world"}
+
+{
+  "hello": "world"
+}
+```
---
+++ bin/buf.pl
@@ -0,0 +1,19 @@
+#!/bin/env perl
+
+use strict;
+use warnings;
+
+my $buf = '';
+my $empty = 0;
+
+while (<>) {
+    $buf .= $_;
+    if (length $_ > 1) {
+        $empty = 0;
+    } else {
+        $empty++;
+    }
+    last if $empty >= 1;
+}
+
+print $buf;
